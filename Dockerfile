4baed9cc6b8e46a721eac5f5f3f1be6e3b16ab25
Jordan Bean <jordanbean@microsoft.com>
2023-02-14 10:03:41 -0600

    init

---
+++ Dockerfile
@@ -0,0 +1,20 @@
+FROM python:3.8-slim-buster
+
+LABEL "com.github.actions.name"="WTH Spell Check Action"
+LABEL "com.github.actions.description"="Check spelling of Markdown files in the WhatTheHack repo"
+LABEL "com.github.actions.icon"="clipboard"
+LABEL "com.github.actions.color"="green"
+LABEL "repository"="http://github.com/jordanbean-msft/wth-spell-check-action"
+LABEL "homepage"="http://github.com/actions"
+LABEL "maintainer"="Jordan Bean <jordanbean@microsoft.com>"
+
+RUN apt-get update \
+  && apt-get install -y aspell hunspell
+
+RUN pip3 install pyspelling pyyaml
+
+COPY generate-spellcheck.py /generate-spellcheck.py
+COPY entrypoint.sh /entrypoint.sh
+RUN chmod +x /entrypoint.sh
+
+ENTRYPOINT ["/entrypoint.sh"]
---
+++ generate-spellcheck.py
@@ -0,0 +1,23 @@
+import sys
+import yaml
+import io
+import os
+
+CUSTOM_WORD_LIST_FILENAME = '.wordlist.txt'
+
+def find_wordlist_files(path):
+    wordlist_paths = []
+    for root, dirs, files in os.walk(path):
+        for file in files:
+            if file.endswith(CUSTOM_WORD_LIST_FILENAME):
+                wordlist_paths.append(os.path.join(root, file))
+    return wordlist_paths
+    
+spell_check_yaml_path = sys.argv[1]
+markdown_base_path = sys.argv[2]
+
+with open(spell_check_yaml_path, 'rw') as file:
+    spell_check_yaml = yaml.load(file, Loader=yaml.SafeLoader)
+    wordlist_paths = find_wordlist_files(markdown_base_path)
+    spell_check_yaml.matrix['Markdown'].dictionary.wordlists.append(wordlist_paths)
+    yaml.dump(file, spell_check_yaml)
